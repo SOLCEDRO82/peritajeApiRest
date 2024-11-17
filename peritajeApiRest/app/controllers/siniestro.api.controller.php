80637102ffb1a2c12e06e9c9b880b37006f116a2
solcedro82 <solcedro82@gmail.com>
2024-11-17 15:12:56 -0300

    carpetas

--- peritajeApiRest/app/controllers/siniestro.api.controller.php
+++ peritajeApiRest/app/controllers/siniestro.api.controller.php
@@ -12,15 +12,15 @@ class SiniestroApiController{
     }
 
     //obtengo TODOS los siniestros de la db
-    // va a ser llamado desde /api/siniestros
+    // (/api/siniestros)
     public function getAll($req, $res){
         $siniestros = $this->model->getSiniestros();
         //los mando a la vista
         return $this->view->response($siniestros);
     }
 
-    //obtengo UN sinierstro en particular
-    //esto va a ser llamado desde algo como /api/siniestros/:id
+    //obtengo UN siniestro en particular
+    // (api/siniestros/:id)
     public function get($req, $res){
         //obtengo el id del siniestro de los parametros desde la ruta
         $id= $req->params->id;
